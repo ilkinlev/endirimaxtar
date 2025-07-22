cced4a9a411c639a4d34df9316bc6d10c2cb8219
Hajiyev Ilkin <arcrutus007@gmail.com>
2025-07-22 17:06:31 +0400

    FirstUpdate

---
+++ app/productInfo.tsx
@@ -0,0 +1,11 @@
+export default function Getproduct() {
+  return (
+    <>
+      <main className="flex ">
+        <div>Product Name</div>
+        <div>Product Details</div>
+        <div>Market</div>
+      </main>
+    </>
+  );
+}
